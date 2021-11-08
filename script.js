3b68dcffca164e0cc1bb841fc500d0012a385ac3
anisirsane <anis.irsane2001@gmail.com>
2021-11-08 05:07:03 -0500

    adding the other functionsnflipcard and checkFOrMatch

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ document.addEventListener('DOMContentLoaded',()=>{
 
 	},
 	{
-		name:'cheesburger',
-		img:'images/cheesburger.png'
+		name:'cheeseburger',
+		img:'images/cheeseburger.png'
 
 	},
 	{
@@ -38,16 +38,34 @@ document.addEventListener('DOMContentLoaded',()=>{
 	}
 	]
 	const grid = document.querySelector('.grid')
+	var cardsChosen = []
+	var cardsChosenId= []
 	//create the board 
 	function createBoard(){
 		for (let i=0 ; i<=cardArray.length; i++){
 			var card = document.createElement('img')
 			card.setAttribute('src', 'images/blank.png')
 			card.setAttribute('data-id', i)
-			//card.addEventListener('click',flipcard)
+			card.addEventListener('click',flipcard)
 			grid.appendChild(card)
 
 		}
+	function checkForMatch(){
+		var cards = document.querySelectorAll('img')
+		const optionOneId = cardsChosenId[0]
+		if (cardsChosen[0] === cardsChosen[1]){
+			alert('you found a match')
+		}
+	}
+	function flipCard(){
+		var cardId = this.getAttribute('data-id')
+		cardsChosen.push(cardArray[cardId].name)
+		cardsChosenId.push(cardId)
+		this.setAttribute('src', cardArray[cardId].img)
+		if (cardsChosen.length === 2){
+			setTimeout(checkForMatch, 500)
+		}
+	}
 
 
 	}
